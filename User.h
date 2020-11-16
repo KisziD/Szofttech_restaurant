aca2f323c516ce0951910cf0d71f831098988f93
Dominik Kiszlinger <kiszidomi1@gmail.com>
2020-11-16 10:54:51 +0100

    User osztály létrehozva

---
+++ User.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+class User
+{
+private:
+
+	string name, email, phone, userName, password;
+	bool admin;
+
+public:
+
+	User();
+};
+
