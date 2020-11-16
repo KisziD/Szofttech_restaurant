63240b435fa7536b2829fe7584baece2b630af59
KisziD <kiszidomi1@gmail.com>
2020-11-16 13:57:48 +0100

    Revert "Hatalmas hiba"
    This reverts commit 527d705adcc900ee8626be24809be424dea0b489.

--- User.h
+++ User.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <stdio.h>
 #include <string>
-#include <vector>
-#include "Reservation.h"
 
 using namespace std;
 class User
@@ -11,20 +9,9 @@ private:
 
 	string name, email, phone, userName, password;
 	bool admin;
-	vector<Reservation> reservations;
 
 public:
 
-	User(string n, string e, string p, string u, string pass, bool a);
-	string getName();
-	void setName(string n);
-	string getEmail();
-	void setEmail(string e);
-	string getPhone();
-	void setPhone(string p);
-	string getUName();
-	void setUName(string u);
-	bool getAdmin();
-	void setAdmin(bool a);
+	User();
 };
 
